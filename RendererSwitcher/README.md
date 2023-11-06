9ccf963e46fd71b9c7a4c43dd8332891e7ddc101
Bugadinho <miguelriechi_windows8@outlook.com>
2023-11-06 00:07:48 -0300

    Add RendererSwitcher

---
+++ RendererSwitcher/README.md
@@ -0,0 +1,2 @@
+# RendererSwitcher
+RendererSwitcher is a simple plugin that allows Roblox's renderer to be changed by altering the `FORCE_RENDERER` environment variable.
---
+++ RendererSwitcher/plugin.lua
@@ -0,0 +1,22 @@
+validRenderers = {
+	["OpenGL"] = true,
+	["Vulkan"] = true,
+	["D3D11FL10"] = true,
+	["D3D11"] = true
+}
+
+function PluginVersion()
+	local rendererOverride = GetEnvironment("FORCE_RENDERER")
+	if rendererOverride ~= "" and validRenderers[rendererOverride] == true then
+		Log("Overriding renderer to " .. rendererOverride .. "!", "info")
+		for renderer,_ in pairs(validRenderers) do
+			if renderer == rendererOverride then
+				SetFFlag("FFlagDebugGraphicsPrefer" .. renderer, "true")
+				SetFFlag("FFlagDebugGraphicsDisable" .. renderer, "false")
+			else
+				SetFFlag("FFlagDebugGraphicsPrefer" .. renderer, "false")
+				SetFFlag("FFlagDebugGraphicsDisable" .. renderer, "true")
+			end
+		end
+	end
+end
